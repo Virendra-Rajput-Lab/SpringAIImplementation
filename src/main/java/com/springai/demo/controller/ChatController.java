80c2b4103aa4f7ee13b141428117be12ad62848b
tubelig-ht <virendrarajput557@gmail.com>
2025-09-11 19:59:33 +0530

    Cross Origin for frontend

--- src/main/java/com/springai/demo/controller/ChatController.java
+++ src/main/java/com/springai/demo/controller/ChatController.java
@@ -2,10 +2,7 @@ package com.springai.demo.controller;
 
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1.1/chat")
@@ -16,7 +13,7 @@ public class ChatController {
     public ChatController(ChatClient.Builder chatClient) {
         this.chatClient = chatClient.build();
     }
-
+    @CrossOrigin(origins = "*")
     @GetMapping("/")
     public ResponseEntity<String> chat(@RequestParam(value="query") String query){
         var response = chatClient.prompt(query).call().content();
